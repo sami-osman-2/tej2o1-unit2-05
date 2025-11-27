078f3a3a8effb034d98f1c4958f6f080c78b535f
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-11-26 23:40:27 -0500

    added to makerspace

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
-basic.forever(function () {
-	
+/* Copyright (c) 2025 MTHS All rights reserved
+ *
+ * Created by: Sami Osman
+ * Created on: Sep 2025
+ * This program gets the current tempurature in Celcius, and converts to Kelvin.
+*/
+
+// variable for temperature
+let tempC: number
+let tempK: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // get temperature in Celsius
+    tempC = input.temperature()
+
+    // convert to Kelvin
+    tempK = tempC + 273.15
+    tempK = Math.round(tempK)
+
+    // output answer
+    basic.clearScreen()
+    basic.showString(tempK + " K")
 })
